a7a4b08c9e7b7b7a5f6e1297301e43925f25e3d2
haeseok <haeseok_@naver.com>
2025-11-11 16:00:54 +0900

    first commit

---
+++ src/main/java/gg/suddeninfo/SuddenInfoApplication.java
@@ -0,0 +1,13 @@
+package gg.suddeninfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuddenInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuddenInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gg/suddeninfo/SuddenInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.suddeninfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuddenInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
