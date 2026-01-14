f2fea0becf5f41c078ddd0f7413799b7767cfbad
haeseok <haeseok_@naver.com>
2026-01-14 15:05:35 +0900

    생성일, 수정일 관련 어노테이션 추가

--- src/main/java/gg/suddeninfo/SuddenInfoApplication.java
+++ src/main/java/gg/suddeninfo/SuddenInfoApplication.java
@@ -2,8 +2,10 @@ package gg.suddeninfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SuddenInfoApplication {
 
     public static void main(String[] args) {
